a685d34b0d10babe45e119f7441fd5c6379a0fcd
JasonChen2118 <98323610+JasonChen2118@users.noreply.github.com>
2022-03-28 11:17:28 +0800

    Add files via upload
    Initialize

---
+++ singleMutationDetect.py
@@ -0,0 +1,12 @@
+# input DNA sequence
+# randomly generate a single mutation
+# detect the mutation
+# output the original gene and mutated gene
+# output the result of detecting mutation
+
+
+import random
+
+codon = {'TTT': 'Phe', 'TTC': 'Phe', 'TTA': 'Leu', 'TTG': 'Leu'
+
+         }
